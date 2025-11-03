4d8ef2c46a337318e639e219247f74cec8ff6ca6
cyanezf <cyanezf@yf-controls.com>
2025-11-03 19:32:20 +0100

    Frontend: Creating device-control component

--- frontend/src/app/devices/components/device-control/device-control.component.ts
+++ frontend/src/app/devices/components/device-control/device-control.component.ts
@@ -1,28 +1,28 @@
+import { NgClass } from '@angular/common';
 import { Component, input, signal } from '@angular/core';
-
 
 
 @Component({
   selector: 'app-device-control',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './device-control.component.html',
 })
 export class DeviceControlComponent {
 
-  name = input.required<string>();
-  status = signal<string>('?');
-
-  updateStatus () {
-
-  }
-
-
+  inName = input.required<string>();
+  inDescription = input.required<string>();
+  inStatus = input.required<'unknown' | 'off' | 'on'>();
+  
+  
   setOn () {
-
+    console.log(`${this.inName()} sets ON`);
   }
 
   setOff () {
-
+    console.log(`${this.inName()} sets OFF`);
   }
 
+  clear() {
+    console.log(`${this.inName()} clear`);
+  }
  }
